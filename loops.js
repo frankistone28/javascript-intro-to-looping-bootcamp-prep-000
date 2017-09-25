c2693db139e61a806325425da15511167d29e8ae
Franklyn Thompson <frankistone28@outlook.com>
2017-09-25 15:53:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array){
+    for(let i = 0; i < 25; i++){
+        array.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`);
+    }
+    return array;
+}
+
+function whileLoop(number){
+  while (number > 0) {
+    console.log(--number);
+  }
+  return "done";
+}
